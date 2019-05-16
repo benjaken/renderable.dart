8cfc9e83fe729fa2e8101dd709474d294c68dce9
copydog <775150@qq.com>
2019-05-16 14:37:18 +0800

    Initial Commit

---
+++ example/code_gen_example.dart
@@ -0,0 +1,6 @@
+import 'package:code_gen/code_gen.dart';
+
+main() {
+  var awesome = new Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/code_gen.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library code_gen;
+
+export 'src/code_gen_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
