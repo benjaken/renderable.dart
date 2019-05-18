b11b20f8df3c985cb771dd0ff321b6f063a06992
copydog <775150@qq.com>
2019-05-18 14:43:16 +0800

    demo: update

---
+++ demo/lib/src/origin_class.mirror.dart
@@ -0,0 +1,20 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// MirrorGenerator
+// **************************************************************************
+
+import 'package:demo/annotation/mirror.dart';
+import 'package:rxdart/rxdart.dart' as rx show Observable hide PublishSubject;
+
+abstract class Interface1 extends Object {}
+
+abstract class Interface2 extends Object {}
+
+class OriginClass extends Object implements Interface1, Interface2 {
+  Map<List<Future<int>>, int> nestedField;
+
+  rx.Observable<int> prefixedField;
+
+  rx.Observable<rx.Observable<dynamic>> nestedPrefixedField;
+}
